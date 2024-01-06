b3337cb35153c64ab9b268baa201fd2108424308
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-01-06 19:49:24 +0530

    server side

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import FetchData from './FetchData';
+const express = require("express");
+const app = express();
+const fs = require("fs");
 
-function App() {
- return (
-    <div className="App">
-      <FetchData />
-    </div>
- );
-}
+app.get("/data", (req, res) => {
+    fs.readFile("data.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error reading JSON file");
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 
-export default App;
+app.listen(3001, () => {
+    console.log("Express server listening on port 3001");
+});
